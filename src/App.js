42ac5b35f47793207dc1a92105c5778df3b89b6c
NikkiSharma-1 <114333988+NikkiSharma-1@users.noreply.github.com>
2023-02-01 14:40:14 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,51 @@
 
 import ExpenseItem from "./components/ExpenseItem";
 function App() {
+  const expense=[
+    {
+  title:'Toilet Paper',
+  amount:300,
+  date:new Date(2022,2,28),
+  location:'USA'
+  },
+  {
+    title:'New TV',
+    amount:788,
+    date:new Date(2022,2,28),
+    location:'Canada'
+    },
+  {
+    title:'Car Insurance',
+    amount:269,
+    date:new Date(2022,2,28),
+    location:'Houston'
+    },
+    {
+      title:'New desk',
+      amount:159,
+      date:new Date(2022,2,28),
+      location:'USA'
+      } 
+]
   return (
     <div>
       <h2>Let's started...</h2>
-      <ExpenseItem></ExpenseItem>
+      <ExpenseItem title={expense[0].title}
+      amount={expense[0].amount}
+    date={expense[0].date}
+    location={expense[0].location}></ExpenseItem>
+<ExpenseItem title={expense[1].title}
+      amount={expense[1].amount}
+    date={expense[1].date}
+    location={expense[1].location}></ExpenseItem>
+    <ExpenseItem title={expense[2].title}
+      amount={expense[2].amount}
+    date={expense[2].date}
+    location={expense[2].location}></ExpenseItem>
+    <ExpenseItem title={expense[3].title}
+      amount={expense[3].amount}
+    date={expense[3].date}
+    location={expense[3].location}></ExpenseItem>
     </div>
   );
 }
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,17 +1,14 @@
 import './ExpenseItem.css';
 
-function ExpenseItem(){
-    const expenseDate=new Date(2022,2,28);
-    const expenseDescription='Car Insurance';
-    const expensePrice=269;
-    const expenseLocation='USA';
+function ExpenseItem(props){
+    
     return (
         <div className="expense-item">
-        <div>{expenseDate.toISOString()}</div>
+        <div>{props.date.toISOString()}</div>
         <div className="expense-item__description">
-        <h2>{expenseDescription}</h2>
-        <div className="expense-item__price">${expensePrice}</div>
-        <div>{expenseLocation}</div>
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
+        <div>{props.location}</div>
         </div> 
     </div>
     ); 
