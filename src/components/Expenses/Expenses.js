76f17b28dbacd8589525e58e3cddcff95afc8aec
NikkiSharma-1 <114333988+NikkiSharma-1@users.noreply.github.com>
2023-02-06 21:05:41 +0530

    commit

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -12,9 +12,24 @@ const Expenses = (props) => {
     setFilteredYear(selectedYear);
   };
 
-  const filterExpenses=props.item.filter((expense) => {
+  const filteredExpenses = props.items.filter((expense) => {
     return expense.date.getFullYear().toString() === filteredYear;
-  })
+  });
+
+  let expensesContent = <p>No expenses found.</p>;
+
+  if (filteredExpenses.length > 0) {
+    expensesContent = filteredExpenses.map((expense) => (
+      <ExpenseItem
+        key={expense.id}
+        title={expense.title}
+        amount={expense.amount}
+        date={expense.date}
+      />
+    ));
+    <p>Add More Expenses</p>
+    
+  }
 
   return (
     <div>
@@ -23,13 +38,7 @@ const Expenses = (props) => {
           selected={filteredYear}
           onChangeFilter={filterChangeHandler}
         />
-        {filterExpenses.map((expense) => (
-          <ExpenseItem
-            title={expense.title}
-            amount={expense.amount}
-            date={expense.date}
-          />
-        ))}
+        {expensesContent}
       </Card>
     </div>
   );
