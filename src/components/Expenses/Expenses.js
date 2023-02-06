75d618162d8216c60de0af6867b85d796e08d438
NikkiSharma-1 <114333988+NikkiSharma-1@users.noreply.github.com>
2023-02-06 20:49:36 +0530

    commit

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -12,6 +12,10 @@ const Expenses = (props) => {
     setFilteredYear(selectedYear);
   };
 
+  const filterExpenses=props.item.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  })
+
   return (
     <div>
       <Card className='expenses'>
@@ -19,7 +23,7 @@ const Expenses = (props) => {
           selected={filteredYear}
           onChangeFilter={filterChangeHandler}
         />
-        {props.items.map((expense) => (
+        {filterExpenses.map((expense) => (
           <ExpenseItem
             title={expense.title}
             amount={expense.amount}
